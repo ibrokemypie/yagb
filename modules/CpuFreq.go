6867b0ade222218f39a2b26692017a5dae09d597
ibrokemypie <ibrokemypie@bastardi.net>
2017-08-08 19:07:18 +1000

    modules/CpuFreq: better defaults adds percent sign and changes precision to one.

--- modules/CpuFreq.go
+++ modules/CpuFreq.go
@@ -11,12 +11,12 @@ import (
 func CpuFreq(channel chan string) {
 	var (
 		interval  = 1000
-		precision = 2
+		precision = 1
 	)
 
 	for {
 		p, _ := cpu.Percent(time.Duration(0)*time.Millisecond, false)
-		channel <- strconv.FormatFloat(p[0], 'f', precision, 64)
+		channel <- strconv.FormatFloat(p[0], 'f', precision, 64)+"%%"
 		time.Sleep(time.Duration(interval) * time.Millisecond)
 	}
 }

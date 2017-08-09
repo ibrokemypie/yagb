6465b74a2c990ebd5d62f145bd38d9f03523cad1
ibrokemypie <ibrokemypie@bastardi.net>
2017-08-09 18:44:10 +1000

    Modules/CpuFreq: move rounding function to common file

--- modules/CpuFreq.go
+++ modules/CpuFreq.go
@@ -20,11 +20,3 @@ func CpuFreq(channel chan string) {
 		time.Sleep(time.Duration(interval) * time.Millisecond)
 	}
 }
-
-func round(v float64, decimals int) float64 {
-	var pow float64 = 1
-	for i := 0; i < decimals; i++ {
-		pow *= 10
-	}
-	return float64(int((v*pow)+0.5)) / pow
-}
